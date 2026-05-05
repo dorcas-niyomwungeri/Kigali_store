eb5b493ff15c60acfb7f0c5394232b26c56323b8
Dorcas Niyomwungeri <dorcasniyomwungeri08@gmail.com>
2026-05-05 08:24:39 +0200

    structu

--- src/assets/router/index.js
+++ src/assets/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   },
   {
     path: '/wishlist',
-    component: () => import('@/views/wishlist.vue')
+    component: () => import('@/views/Wishlist.vue')
   },
   {
     path: '/checkout',
